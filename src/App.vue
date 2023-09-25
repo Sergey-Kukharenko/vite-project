fc0a4d06d15ec5a631d694ee07638e18e3baab6a
Sergey Kukharenko <iamksergey@mail.ru>
2023-09-25 17:49:26 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import AppBtn from './components/app-btn.vue';
+</script>
+
+<template>
+  <div class="row-example">
+    <app-btn disabled>button</app-btn>
+    <app-btn size="small">button</app-btn>
+    <app-btn>button</app-btn>
+    <app-btn size="large">button</app-btn>
+  </div>
+  <div class="row-example">
+    <app-btn disabled appearance="ghost">button</app-btn>
+    <app-btn appearance="ghost" size="small">button</app-btn>
+    <app-btn appearance="ghost"> button</app-btn>
+    <app-btn appearance="ghost" size="large">button</app-btn>
+  </div>
+  <div class="row-example">
+    <h1>This is h1 tag</h1>
+  </div>
+  <div class="row-example">
+    <h2>This is h2 tag</h2>
+  </div>
+</template>
---
+++ src/components/app-btn.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+
+  appearance: {
+    type: String,
+    default: 'yellow',
+    validator(value) {
+      return ['yellow', 'ghost'].includes(value);
+    },
+  },
+
+  size: {
+    type: String,
+    default: 'medium',
+    validator(value) {
+      return ['small', 'medium', 'large'].includes(value);
+    },
+  },
+});
+
+const classNames = computed(() => ({
+  btn: true,
+  [`btn--${props.appearance}`]: props.appearance,
+  [`btn--${props.size}`]: props.size,
+}));
+</script>
+
+<template>
+  <button :class="classNames" :disabled="disabled">
+    <slot />
+  </button>
+</template>
+
+<style scoped></style>
