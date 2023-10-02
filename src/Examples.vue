8a84a0fa884d8c1c7752765b8c61c18998311bed
Sergey Kukharenko <iamksergey@mail.ru>
2023-10-02 15:53:17 +0300

    init

---
+++ src/Examples.vue
@@ -0,0 +1,61 @@
+<script setup>
+import AppBtn from './components/btn/AppBtn.vue';
+</script>
+
+<template>
+  <div class="row-example">
+    <app-btn tag="a">link</app-btn>
+    <app-btn tag="a" href="/some" target="_blank">link</app-btn>
+  </div>
+  <div class="row-example">
+    <app-btn disabled>button</app-btn>
+    <app-btn size="small">button</app-btn>
+    <app-btn>button</app-btn>
+    <app-btn size="large">button</app-btn>
+  </div>
+  <div class="row-example">
+    <app-btn disabled appearance="ghost">button</app-btn>
+    <app-btn appearance="ghost" size="small">button</app-btn>
+    <app-btn appearance="ghost"> button</app-btn>
+    <app-btn appearance="ghost" size="large">button</app-btn>
+  </div>
+  <div class="row-example">
+    <h1>This is h1 tag</h1>
+  </div>
+  <div class="row-example">
+    <h2>This is h2 tag</h2>
+  </div>
+  <div class="row-example">
+    <h3>This is h3 tag</h3>
+  </div>
+  <div class="row-example">
+    <h4>This is h4 tag</h4>
+  </div>
+  <div class="row-example">
+    <div class="text-xxl">This is an example text.</div>
+  </div>
+  <div class="row-example">
+    <div class="text-xl">This is an example text.</div>
+  </div>
+  <div class="row-example">
+    <div class="text-lg">This is an example text.</div>
+  </div>
+  <div class="row-example">
+    <p>This is an example text.</p>
+  </div>
+  <div class="row-example">
+    <div class="text-sm">This is an example text.</div>
+  </div>
+  <div class="row-example">
+    <div class="text-xs">This is an example text.</div>
+  </div>
+  <div class="row-example">
+    <div class="text-xs text-error">This is an example text.</div>
+  </div>
+  <div class="row-example">
+    <div class="text-xs text-grey">This is an example text.</div>
+  </div>
+  <div class="row-example">
+    <a href="#">This is an example text.</a>
+  </div>
+</template>
---
+++ src/components/banner/banner-card/AppBannerCard.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <div class="banner-card">
+    <img src="https://placehold.co/1222X403.jpg" class="banner-card__img" alt="" />
+  </div>
+</template>
+
+<style scoped lang="css">
+.banner-card {
+  background: #005be4;
+
+  @media screen and (min-width: 768px) {
+    min-height: 403px;
+  }
+
+  @media screen and (max-width: 767px) {
+    min-height: 384px;
+  }
+}
+
+.banner-card__img {
+  display: block;
+  max-width: 100%;
+}
+</style>
