fbae5478103bd081d1853e5fd65490b6b7bf7aac
Sergey Kukharenko <iamksergey@mail.ru>
2023-10-04 16:32:35 +0300

    init

--- src/components/banner/banner-card/AppBannerCard.vue
+++ src/components/banner/banner-card/AppBannerCard.vue
@@ -1,15 +1,20 @@
-<script setup></script>
+<script setup>
+import { computed } from 'vue';
+import { useMq } from 'vue3-mq';
+
+const mq = useMq();
+const getImg = computed(() => mq.current);
+</script>
 
 <template>
   <div class="banner-card">
+    <pre>{{ getImg }}</pre>
     <img src="https://placehold.co/1222X403.jpg" class="banner-card__img" alt="" />
   </div>
 </template>
 
 <style scoped lang="css">
 .banner-card {
-  background: #005be4;
-
   @media screen and (min-width: 768px) {
     min-height: 403px;
   }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
+import { Vue3Mq } from 'vue3-mq';
+
 import './assets/css/reset.css';
 import './assets/css/normalize.css';
 import './style.css';
+
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(Vue3Mq, {
+    breakpoints: {
+      mobile: 0,
+      tablet: 768,
+      desktop: 1280
+    }
+  })
+  .mount('#app');
